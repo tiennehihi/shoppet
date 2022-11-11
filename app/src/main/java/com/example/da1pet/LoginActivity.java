4d02510db81d1455c8fe2c134e46ea1b7e74786d
Dai <ngkhacdai@gmail.com>
2022-11-11 08:55:07 +0700

    dai giao dien home 2

--- app/src/main/java/com/example/da1pet/LoginActivity.java
+++ app/src/main/java/com/example/da1pet/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.da1pet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        findViewById(R.id.btnback).setOnClickListener(v -> {
+            Intent intent = new Intent(this,LaucherActivity.class);
+            startActivity(intent);
+        });
     }
 }
