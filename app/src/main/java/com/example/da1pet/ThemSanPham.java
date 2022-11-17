b50986f78a950e591fb374ab4874b71e4b0044c0
Dai <ngkhacdai@gmail.com>
2022-11-17 14:51:38 +0700

    update them san pham

--- app/src/main/java/com/example/da1pet/ThemSanPham.java
+++ app/src/main/java/com/example/da1pet/ThemSanPham.java
@@ -1,11 +1,26 @@
 package com.example.da1pet;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class ThemSanPham extends AppCompatActivity {
+import android.widget.ImageView;
+import android.widget.Toast;
 
+;
+
+import java.util.List;
+
+
+public class ThemSanPham extends AppCompatActivity {
+    int SELECT_IMAGE_CODE = 1;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +28,26 @@ public class ThemSanPham extends AppCompatActivity {
         findViewById(R.id.btnback).setOnClickListener(v -> {
             onBackPressed();
         });
+        img = findViewById(R.id.imgpro);
+        findViewById(R.id.btnpicpr).setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent,"title"),SELECT_IMAGE_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1){
+            try {
+                Uri uri = data.getData();
+                img.setImageURI(uri);
+            }catch (Exception e){
+                e.getMessage();
+            }
+
+        }
     }
 }
