2d2418f9f94edc514f2c17a775f0e84305219de3
LUC TUNG <tungltph25765@fpt.edu.vn>
2022-11-21 08:19:22 +0700

    them sk click gio hang

--- app/src/main/java/com/example/da1pet/Shop/Shoppet.java
+++ app/src/main/java/com/example/da1pet/Shop/Shoppet.java
@@ -1,17 +1,24 @@
 package com.example.da1pet.Shop;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.ImageButton;
 import com.example.da1pet.R;
-
+import com.example.da1pet.home.HomeFragment;
 public class Shoppet extends AppCompatActivity {
-
+   ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoppet);
-        //
+
+
+
+      imageButton = findViewById(R.id.btnbackHome);
+      imageButton.setOnClickListener(v -> {
+          Intent intent = new Intent(this, HomeFragment.class);
+             startActivity(intent);
+       });
+
     }
 }
--- app/src/main/java/com/example/da1pet/home/HomeFragment.java
+++ app/src/main/java/com/example/da1pet/home/HomeFragment.java
@@ -1,9 +1,6 @@
 package com.example.da1pet.home;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
@@ -11,74 +8,48 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.example.da1pet.DbRoom.DbRoom;
-import com.example.da1pet.LaucherActivity;
-import com.example.da1pet.Model.Products;
-import com.example.da1pet.ProductActivity;
 import com.example.da1pet.R;
+import com.example.da1pet.Shop.Shoppet;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-    Context context;
-    ArrayList<Products> list;
-    DbRoom db;
-    Button btnXem1;
-    NonScrollGridView gridView;
-    String TAG = "zzzzz";
-
-    //    public HomeFragment(Context context, List<Products> list) {
-    //        this.context = context;
-    //        this.list = list;
-    //        this.db = DbRoom.getInstance(context);
-    //    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        db = DbRoom.getInstance(this.getContext());
         ImageView slideshow = view.findViewById(R.id.homeslideshow);
         slideshow.setBackgroundResource(R.drawable.home_slideshow);
         AnimationDrawable animationDrawable = (AnimationDrawable) slideshow.getBackground();
         animationDrawable.start();
-        list = new ArrayList<>();
-        try {
-                list = (ArrayList<Products>) db.productsDAO().getAll();
-                GrAdapter adapter = new GrAdapter(list);
-                gridView = view.findViewById(R.id.nonscrollgridview);
-                gridView.setAdapter(adapter);
-                gridView.setNumColumns(2);
-                gridView.setExpanded(true);
-        }catch (Exception e){
-            Log.e(TAG, "onViewCreated: "+e.getMessage());
-        }
+        ImageButton btn = view.findViewById(R.id.btnshop);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(this.getActivity(), Shoppet.class);
+            startActivity(intent);
+        });
 
+//        NonScrollGridView gridView = view.findViewById(R.id.lv);
+//        gridView.setNumColumns(2);
+//        gridView.setAdapter(new GrAdapter(list));
+//        gridView.setExpanded(true);
     }
-
-
-
-
     public class GrAdapter extends BaseAdapter {
-        ArrayList<Products> list;
+        ArrayList<Integer> list;
 
-        public GrAdapter(ArrayList<Products> list) {
+        public GrAdapter(ArrayList<Integer> list) {
             this.list = list;
         }
 
@@ -99,26 +70,9 @@ public class HomeFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.progrid,parent,false);
-            try {
-                ImageView imageView = convertView.findViewById(R.id.imggrid);
-                TextView textView = convertView.findViewById(R.id.tenspgrid);
-                TextView textView1 = convertView.findViewById(R.id.giagrid);
-                Products products = list.get(position);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(products.getImg_product(), 0, products.getImg_product().length);
-                imageView.setImageBitmap(bitmap);
-                textView.setText(products.getName_products());
-                textView1.setText(String.valueOf(products.getPrice()));
-            }catch (Exception e){
-                e.getMessage();
-            }
-            convertView.setOnClickListener(v -> {
-                Intent intent = new Intent(HomeFragment.this.getActivity(), ProductActivity.class);
-                startActivity(intent);
-            });
+
             return convertView;
         }
     }
-
 
 }
